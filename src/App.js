78a0421b41b0cd16134c67d779ded8654d296a1f
AmanWorku <amexworku2014@gmail.com>
2023-07-12 09:38:23 +0300

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Greeting from "./components/Greeting";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Greeting from './components/Greeting';
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route exact path="/" component={Greeting} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" component={Greeting} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,27 +1,28 @@
-import React, { useEffect } from "react";
-import { fetchGreetings } from "../store/greetingsSlice";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreetings } from '../store/greetingsSlice';
 
 function Greetings() {
-	const dispatch = useDispatch();
-	const greetingsData = useSelector((state) => state.greetings.greetings);
-	const singleGreeting =
-		greetingsData[Math.floor(Math.random() * greetingsData.length)];
+  const dispatch = useDispatch();
+  const greetingsData = useSelector((state) => state.greetings.greetings);
+  const singleGreeting = greetingsData[Math.floor(Math.random() * greetingsData.length)];
 
-	useEffect(() => {
-		dispatch(fetchGreetings());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchGreetings());
+  }, [dispatch]);
 
-	return (
-		<div className="center">
-			<button
-				onClick={() => dispatch(fetchGreetings())}
-				className="greeting-btn">
-				Press Me
-			</button>
-			{singleGreeting && <h1>{singleGreeting.greeting}</h1>}
-		</div>
-	);
+  return (
+    <div className="center">
+      <button
+        type="button"
+        onClick={() => dispatch(fetchGreetings())}
+        className="greeting-btn"
+      >
+        Press Me
+      </button>
+      {singleGreeting && <h1>{singleGreeting.greeting}</h1>}
+    </div>
+  );
 }
 
 export default Greetings;
